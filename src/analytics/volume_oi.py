e6d16d146feaab051ade6d02b8fadb296a98dc36
ushalbansode-ai <ushalbansode@gmail.com>
2025-12-02 10:32:13 +0530

    Update volume_oi.py

--- src/analytics/volume_oi.py
+++ src/analytics/volume_oi.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from .utils import zscore
 
+def detect_volume_spike(df: pd.DataFrame) -> pd.Series:
+    """
+    Detects abnormal volume spikes vs historical mean.
+    """
+    vol = df["Volume"]
+    return zscore(vol)
+  
